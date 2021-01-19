148a15aa5b0cdc88ccb9854dc15e63916c72bbb4
Gabrielle <linda41436@gmail.com>
2021-01-19 11:59:14 +0800

    Finished index.html, create Component of "Product Card". Update layout settings.

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,12 @@
-var el = document.getElementsByClassName('like');
-
-for (i=0; i< el.length; i++) {
-    el[i].addEventListener('click',likeit,false);
-};
-
-function likeit(e){
-    e.preventDefault();
-    alert('I like it!');
-};
+var heart = document.getElementsByClassName('likeBtn');
+for (i=0; i<heart.length; i++){
+    heart[i].addEventListener('click', function (){
+        if(this.className === 'likeBtn'){
+            this.setAttribute('src', 'images/heart-solid.svg');
+            this.setAttribute('class', 'likeBtn liked');
+        }else{
+            this.setAttribute('src', 'images/heart-regular.svg');
+            this.setAttribute('class', 'likeBtn');
+        }
+    },false);
+}
