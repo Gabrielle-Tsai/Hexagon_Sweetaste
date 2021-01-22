667b247e6fa9b8a9ea6288d2b6f4b39b115ea9a0
Gabrielle <linda41436@gmail.com>
2021-01-22 18:47:02 +0800

    Finished cart.html, checkout1.html. Add checkout2.html. Renewed customComponents setting.

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,20 @@
 var banner = document.getElementsByClassName('bg-banner');
-var checkPage = banner[0].getAttribute('id');
-var changebg = document.getElementById(checkPage);
-console.log(changebg);
-switch(checkPage) {
-    case "indexBanner":
-        changebg.style['background-image'] = 'url(https://bit.ly/2OhbMHr)';
+switch(banner){
+    case true:
+        var checkPage = banner[0].getAttribute('id');
+        var changebg = document.getElementById(checkPage);
+        console.log(changebg);
+        switch(checkPage) {
+            case "indexBanner":
+                changebg.style['background-image'] = 'url(https://bit.ly/2OhbMHr)';
+                break;
+            case "productBanner":
+                changebg.style['background-image'] = 'url(images/productBanner.jfif)';
+                break;
+        }
+    break;
+    case false:
         break;
-    case "productBanner":
-        changebg.style['background-image'] = 'url(images/productBanner.jfif)';
-        break;
-
 }
 
 var heart = document.getElementsByClassName('likeBtn');
@@ -23,4 +28,23 @@ for (i=0; i<heart.length; i++){
             this.setAttribute('class', 'likeBtn');
         }
     },false);
+}
+var imgCol = document.getElementsByClassName('product-img');
+for (i=0; i< imgCol.length; i++){
+    var item = imgCol[i];
+    var product = item.dataset.product;
+    switch(product){
+        case "1001":
+            item.style['background-image'] = 'url(https://bit.ly/2QvsT63)';
+        break;
+        case "10001":
+            item.style['background-image'] = 'url(https://bit.ly/2zBjQuq)';
+        break;
+        case "10002":
+            item.style['background-image'] = 'url(https://bit.ly/2zBDAxX)';
+        break;
+        case "10003":
+            item.style['background-image'] = 'url(https://bit.ly/2zKOP7w)';
+        break;
+    }
 }
